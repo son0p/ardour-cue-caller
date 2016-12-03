ec754f6853fc87874aa884508e3225fb1b98759e
son0p <fede2001@gmail.com>
2016-12-03 17:22:00 -0500

    elimina codigo sobrante

--- cueCaller.lua
+++ cueCaller.lua
@@ -12,32 +12,12 @@ function factory ()
 			-- not rolling, nothing to do.
 			return
 		end
-
 		local pos = Session:transport_frame () -- current playhead position
-		local loc = Session:locations () -- all marker locations
-
-		-- find first marker after the current playhead position, ignore loop + punch ranges
-		-- (this only works when rolling forward, to extend this example see
-		-- http://manual.ardour.org/lua-scripting/class_reference/#ARDOUR:Locations )
-		local m = loc:first_mark_after (pos, false)
-
-		if (m == -1) then
-			-- no marker was found
-			return
-		end
-
-		-- since ardour may split the process cycle for events,
-		-- n_samples may be smaller.
-		local blk = Session:get_block_size ()
-
     
-
-		-- Transport stop can only happen on a process-cycle boundary.
-		-- This callback happens from within the process callback,
-		-- so we need to queue it ahead of time.
-		if (pos + n_samples + blk >= m and pos + n_samples < m) then
-      Session:route_by_name("foh_audio"):set_comment ("fifi", nil)
+    if(pos > 100000) then
+      Session:route_by_name("foh_audio"):set_comment ("foo", nil)
     end
+    --end
   end
 end
 
